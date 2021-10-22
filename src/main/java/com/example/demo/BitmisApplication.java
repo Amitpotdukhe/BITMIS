a8d2a061650aaacb9cc8083a411bc62cd13281b3
Amit <Amitpotdukhe>
2021-10-22 12:32:37 +0530

    first commit

---
+++ src/main/java/com/example/demo/BitmisApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitmisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitmisApplication.class, args);
+	}
+
+}
