58535c46eb218b2db90208f8b59df687f7ab502b
Bhavesh Anandpara <bhaveshAP09112002@gmail.com>
2021-11-30 08:53:19 +0530

    Common.java Syntax error Solved

--- src/main/java/mis/controller/Common.java
+++ src/main/java/mis/controller/Common.java
@@ -44,7 +44,7 @@ public class Common {
 		return "admin/adminHome";
 	}
 	
-<<<<<<< HEAD
+
 	@RequestMapping("addleavebalance")
 	public String addleavebalance() {
 		return "admin/addleavebalance";
@@ -68,11 +68,12 @@ public class Common {
 	@RequestMapping("manageuser")
 	public String manageuser() {
 		return "admin/manageuser";
-=======
+	}
+
 	@RequestMapping("profile")
 	public String profile() {
 		return "common/profile";
->>>>>>> 7bdcb13634911ce0a455f2de9bf035a344626a3d
+
 	}
 
 }
